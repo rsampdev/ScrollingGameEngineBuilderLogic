b0b56ca7c8e1f5d8f17aaf81189b268cc2cc7466
rsampdev <rsampdev@gmail.com>
2016-09-22 15:46:19 -0400

    Initial Commit

---
+++ ScrollingGameEngineBuilderLogic/ScrollingGameEngineBuilderLogic.h
@@ -0,0 +1,19 @@
+//
+//  ScrollingGameEngineBuilderLogic.h
+//  ScrollingGameEngineBuilderLogic
+//
+//  Created by Rodney Sampson on 9/22/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrollingGameEngineBuilderLogic.
+FOUNDATION_EXPORT double ScrollingGameEngineBuilderLogicVersionNumber;
+
+//! Project version string for ScrollingGameEngineBuilderLogic.
+FOUNDATION_EXPORT const unsigned char ScrollingGameEngineBuilderLogicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollingGameEngineBuilderLogic/PublicHeader.h>
+
+
